5c0b8841f99aec6900ee74b7b1345d414ecdf693
Sven Ruppert <sven.ruppert@gmail.com>
2017-03-07 14:33:15 +0100

    first Client API draft

---
+++ modules/client/api/src/main/java/org/rapidpm/binarycache/client/api/BinaryCacheClient.java
@@ -0,0 +1,58 @@
+package org.rapidpm.binarycache.client.api;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.util.Optional;
+
+/**
+ * Copyright (C) 2017 RapidPM - Sven Ruppert
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by Sven Ruppert - RapidPM - Team on 07.03.17.
+ */
+public interface BinaryCacheClient {
+
+  /* make private with Java9 */
+  default Cache<CacheKey, Byte[]> getCache(String cacheName) {
+    final CachingProvider cachingProvider = Caching.getCachingProvider();
+    final CacheManager cacheManager = cachingProvider.getCacheManager();
+    return cacheManager.getCache(cacheName, CacheKey.class, Byte[].class);
+  }
+
+  default Result cacheBinary(String cacheName, CacheKey cacheKey, Byte[] binary) {
+    final Cache<CacheKey, Byte[]> cache = getCache(cacheName);
+    cache.put(cacheKey,binary);
+    return Result.OK;
+  }
+
+  default Result cacheBinaryIfAbsent(String cacheName, CacheKey cacheKey, Byte[] binary) {
+    final Cache<CacheKey, Byte[]> cache = getCache(cacheName);
+    final boolean ifAbsent = cache.putIfAbsent(cacheKey, binary);
+    return Result.OK;
+  }
+
+  default Optional<Byte[]> getCachedElement(String cacheName, CacheKey cacheKey) {
+    return Optional.ofNullable(getCache(cacheName).get(cacheKey));
+  }
+
+  default Result clearCache(String cacheName) {
+    getCache(cacheName).clear();
+    return Result.OK;
+  }
+
+  default Result removeEntry(String cacheName, CacheKey cacheKey) {
+    final boolean remove = getCache(cacheName).remove(cacheKey);
+    return Result.OK;
+  }
+
+}
