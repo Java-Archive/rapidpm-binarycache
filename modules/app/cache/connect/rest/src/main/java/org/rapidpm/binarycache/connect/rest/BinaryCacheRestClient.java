a422b11e8f47f41025db90a0b3854040cd452c03
Marcus Lang <marcuslang@zoho.com>
2017-03-10 11:51:17 +0100

    WIP cache rest client

--- modules/app/cache/connect/rest/src/main/java/org/rapidpm/binarycache/connect/rest/BinaryCacheRestClient.java
+++ modules/app/cache/connect/rest/src/main/java/org/rapidpm/binarycache/connect/rest/BinaryCacheRestClient.java
@@ -2,12 +2,17 @@ package org.rapidpm.binarycache.connect.rest;
 
 import org.rapidpm.binarycache.api.BinaryCacheClient;
 import org.rapidpm.binarycache.api.CacheKey;
-import org.rapidpm.binarycache.api.Result;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Base64;
 import java.util.Optional;
 
 /**
@@ -21,34 +26,64 @@ import java.util.Optional;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * <p>
  * Created by Sven Ruppert - RapidPM - Team on 08.03.17.
  */
-@Path("/binarycache")
+@Path("/cache")
 public class BinaryCacheRestClient {
 
-  @Inject private BinaryCacheClient binaryCacheClient;
+  @Inject
+  private BinaryCacheClient binaryCacheClient;
 
+  // how to handle really large files
   @POST
-  @Path("/put")
-  public Response cacheBinary(final String cacheName, final CacheKey cacheKey, final Byte[] binary) {
-
+  @Path("/cacheBinary")
+  public Response cacheBinary(@QueryParam("cacheName") final String cacheName,
+                              @QueryParam("key") final String cacheKey,
+                              @QueryParam("value") final String binary) {
     return null;
   }
 
-  public Result cacheBinaryIfAbsent(final String cacheName, final CacheKey cacheKey, final Byte[] binary) {
-    return binaryCacheClient.cacheBinaryIfAbsent(cacheName, cacheKey, binary);
+  @POST
+  @Path("/cacheBinaryIfAbsent")
+  public Response cacheBinaryIfAbsent(@QueryParam("cacheName") final String cacheName,
+                                      @QueryParam("key") final String cacheKey,
+                                      @QueryParam("value") final String binary) {
+    return null;
+    //return binaryCacheClient.cacheBinaryIfAbsent(cacheName, cacheKey, binary);
   }
 
-  public Optional<Byte[]> getCachedElement(final String cacheName, final CacheKey cacheKey) {
-    return binaryCacheClient.getCachedElement(cacheName, cacheKey);
+  @GET
+  @Path("getCachedElement")
+  public Response getCachedElement(@QueryParam("cacheName") final String cacheName,
+                                   @QueryParam("key") final String cacheKey) {
+    final byte[] decode = Base64.getDecoder().decode(cacheKey);
+    try (final ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(decode))) {
+      CacheKey decodedKey = (CacheKey) ois.readObject();
+      final Optional<Byte[]> cachedElement = binaryCacheClient.getCachedElement(cacheName, decodedKey);
+      return Response
+          .status(Response.Status.OK)
+          .entity(cachedElement)
+          .build();
+    } catch (IOException | ClassNotFoundException e) {
+      // logging
+      e.printStackTrace();
+    }
+
+    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
   }
 
-  public Result clearCache(final String cacheName) {
-    return binaryCacheClient.clearCache(cacheName);
+  @GET
+  @Path("clearCache")
+  public Response clearCache(final String cacheName) {
+    return null;
+    //return binaryCacheClient.clearCache(cacheName);
   }
 
-  public Result removeEntry(final String cacheName, final CacheKey cacheKey) {
-    return binaryCacheClient.removeEntry(cacheName, cacheKey);
+  @GET
+  @Path("removeEntry")
+  public Response removeEntry(final String cacheName, final CacheKey cacheKey) {
+    return null;
+    //return binaryCacheClient.removeEntry(cacheName, cacheKey);
   }
 }
